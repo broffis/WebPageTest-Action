4de4cfd6972a1eb9c5ee01e0a289ba71ba42d731
Brandon Roffis <brandon.roffis@ferguson.com>
2023-07-18 11:10:30 -0400

    test with script

--- .github/scripts/get-headers.js
+++ .github/scripts/get-headers.js
@@ -1,9 +1,10 @@
 import fetch from "node-fetch";
 
 module.exports = ({ core }) => {
-  const response = fetch("https://www.build.com");
-  const headers = response.headers;
-  const siteVersion = headers.get("x-build-version");
-
-  core.setOutput("site-version", siteVersion);
+  fetch("https://www.build.com").then((res) => {
+    const { headers } = res;
+    const siteVersion = headers.get("X-Fergy-App-Version");
+    console.log({ headers, siteVersion });
+    core.setOutput("site-version", siteVersion);
+  });
 };
