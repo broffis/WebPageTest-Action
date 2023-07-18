cb3bec6fd26029080f1e6224b434529d029f8f20
Brandon Roffis <brandon.roffis@ferguson.com>
2023-07-18 11:01:28 -0400

    fix(js): use chaining

---
+++ src/fetch.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const fetchData = () => {
+  fetch("https://www.build.com")
+    .then((res) => {
+      console.log(res.headers);
+      const siteVersion = res.headers.get("x-site-version");
+      core.setOutput("site-version", siteVersion);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+fetchData();
